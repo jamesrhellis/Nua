ca27e7070ea421a92d3159cbc868791cb44fbc04
james <james@void.default>
2018-08-21 13:20:22 +0100

    Start tranistion to embeddable api
    Simplified function calling code based on the new api.

---
+++ core_api.h
@@ -0,0 +1,219 @@
+#ifndef NUA_API
+#define NUA_API
+
+typedef struct nua_state {
+	val_al stack;
+	
+	// Mem management
+	size_t white;		// Current val of white tag (0, 1)
+	mem_block *gc_list;	// All objects
+} nua_state;
+
+void gc_mark(nua_state *n, int height) {
+	n->white = !n->white;
+	int white = n->white;
+	
+	for (int i = 0;i < height+1;++i) {
+		gc_val_mark(&n->stack.items[i], !white);
+	}
+}
+
+int nua_call(nua_state *n, int arg_base, int no_args, int no_returns);
+int nua_pcall(nua_state *n, int arg_base, int no_args, int no_returns);
+
+int nua_c_func(nua_state *n, int arg_base, int no_args, int no_returns);
+
+nua_state *nua_init() {
+	nua_state *n = malloc(sizeof(*n));
+	*n = (nua_state) {.stack = val_al_new(256)};
+	for (int i = 0;i < 256;++i) {
+		n->stack.items[i] = (val) { VAL_NIL };
+	}
+	return n;
+}
+
+int nua_call(nua_state *n, int base, int no_args, int no_returns) {	
+	int pc = 0;
+	
+	func *f = n->stack.items[base].func;
+	val *lit = f->def->literals.items;
+	tab *env = f->env;
+	
+	size_t max = base + f->def->max_reg;
+
+	if (max >= n->stack.size) {
+		size_t old = n->stack.size, new = n->stack.size * 2;
+		val_al_resize(&n->stack, new);
+		for (int i = old;i < new;++i) {
+			n->stack.items[i] = (val) {VAL_NIL};
+		}
+	}
+
+	// MAY BE INVALIDATED DURING FUNCTION CALL DUE TO RESIZE
+	val *reg = &n->stack.items[base + 1];
+
+	for (int i = no_args;i < f->def->no_args;++i) {
+		reg[base + i + 1] = (val) { VAL_NIL };
+	}
+
+	while (1) {
+		inst ins = f->def->ins.items[pc];
+		//printf("%d", pc);print_inst(ins);
+		switch (ins.op) {
+		case OP_JMP:
+			pc += ins.off;
+			continue;	// Avoid addition at end of loop
+		case OP_COVER:
+			if (reg[ins.reg].type != VAL_NIL) {
+				pc++;
+			}
+			break;
+		case OP_NIL:
+			reg[ins.reg] = (val) {VAL_NIL};
+			break;
+		case OP_SETL:
+			switch (lit[ins.lit].type) {
+			case VAL_TAB:
+				reg[ins.reg] = (val) {VAL_TAB, .tab = gc_alloc(&global_heap, sizeof(tab), GC_TAB)};
+				reg[ins.reg].tab->al = val_al_clone(&lit[ins.lit].tab->al);
+				reg[ins.reg].tab->ht = val_ht_clone(&lit[ins.lit].tab->ht);
+				break;
+			case VAL_FUNC:
+				reg[ins.reg] = (val) {VAL_FUNC, .func = gc_alloc(&global_heap, sizeof(func), GC_FUNC)};
+				reg[ins.reg].func->type = FUNC_NUA;
+				reg[ins.reg].func->def = lit[ins.lit].func->def;
+				reg[ins.reg].func->env = f->env;
+				break;
+			default:
+				reg[ins.reg] = lit[ins.lit];
+				break;
+			}
+			break;
+		case OP_CALL: {
+			if (reg[ins.rout].type != VAL_FUNC) {
+				printf("Attempt to call non-function!\n");
+				print_val(reg[ins.rout]);
+				return -1;
+			}
+			// OP is interpreted
+			// .rout = func register, and base of func args - 1, base of return vals
+			// .rina = no args, call has to pad with nils
+			// .rinb = no return vals, return has to pad with nils
+			int no_ret;
+			switch (reg[ins.rout].func->type) {
+			case FUNC_NUA:
+				no_ret = nua_call(n, base + 1 + ins.rout, ins.rina, ins.rinb);
+				break;
+			case FUNC_C:	
+				no_ret = reg[ins.rout].func->c_func(ins.rina, &n->stack.items[base + 1 + ins.rout]);				
+				break;
+			default:
+				return -1;
+			}
+
+			val *reg = &n->stack.items[base + 1];	
+									
+			for (int i = no_ret;i < ins.rinb;++i) {
+				reg[ins.rout + i] = (val) { VAL_NIL };
+			}
+			break;
+		} case OP_RET: {
+			// OP is interpreted
+			// .rina = base register
+			// .rout = no values to return
+			for (int i = 0;i < ins.rout;++i) {
+				n->stack.items[base + i] = reg[ins.rina + i];
+			}
+			
+			return ins.rout;
+		} case OP_ADD:
+			if (reg[ins.rina].type == VAL_NUM
+			&&  reg[ins.rinb].type == VAL_NUM) {
+				reg[ins.rout] = (val) {VAL_NUM, reg[ins.rina].num + reg[ins.rinb].num};
+				break;
+			}
+			break;
+		case OP_SUB:
+			if (reg[ins.rina].type == VAL_NUM
+			&&  reg[ins.rinb].type == VAL_NUM) {
+				reg[ins.rout] = (val) {VAL_NUM, reg[ins.rina].num - reg[ins.rinb].num};
+				break;
+			}
+			break;
+		case OP_GT:
+			if (reg[ins.rina].type == VAL_NUM
+			&&  reg[ins.rinb].type == VAL_NUM) {
+				if (reg[ins.rina].num > reg[ins.rinb].num) {
+					reg[ins.rout] = reg[ins.rinb];
+				} else {
+					reg[ins.rout] = (val) {VAL_NIL};
+				}
+				break;
+			}
+			break;
+		case OP_GE:
+			if (reg[ins.rina].type == VAL_NUM
+			&&  reg[ins.rinb].type == VAL_NUM) {
+				if (reg[ins.rina].num >= reg[ins.rinb].num) {
+					reg[ins.rout] = reg[ins.rinb];
+				} else {
+					reg[ins.rout] = (val) {VAL_NIL};
+				}
+			}
+			break;
+		case OP_MOV:
+			reg[ins.rout] = reg[ins.rina];
+			break;
+		case OP_TAB:
+			reg[ins.rout] = (val) {VAL_TAB, .tab = gc_alloc(&global_heap, sizeof(tab), GC_TAB)};
+			val_ht_resize(&reg[ins.rout].tab->ht, ins.rina);
+			val_al_resize(&reg[ins.rout].tab->al, RH_HASH_SIZE(ins.rinb));
+			break;
+		case OP_PTAB:
+			switch (reg[ins.rout].type) {
+			case VAL_TAB:
+				tab_push(reg[ins.rout].tab, reg[ins.rina]);
+				break;
+			default:
+				break;
+			}
+			break;
+		case OP_STAB:
+			switch (reg[ins.rout].type) {
+			case VAL_TAB:
+				tab_set(reg[ins.rout].tab, reg[ins.rina], reg[ins.rinb]);
+				break;
+			default:
+				print_val(reg[ins.rout]);
+				break;
+			}
+			break;
+		case OP_GTAB:
+			switch (reg[ins.rina].type) {
+			case VAL_TAB:
+				reg[ins.rout] = tab_get(reg[ins.rina].tab, reg[ins.rinb]);
+				break;
+			default:
+				break;
+			}
+			break;
+		case OP_SENV:
+			tab_set(env, lit[ins.lit], reg[ins.reg]);
+			break;
+		case OP_GENV:
+			reg[ins.reg] = tab_get(env, lit[ins.lit]);
+			break;
+		case OP_END:
+			return 0;
+		default:
+			break;
+		}
+		gc_mark(n, base + f->def->gc_height.items[pc]);
+		gc_sweep(&global_heap, n->white);
+		
+		pc++;
+	}
+
+	return 0;
+}
+#endif
--- main.c
+++ main.c
@@ -9,59 +9,16 @@
 #include "val.h"
 #include "parse.h"
 
-typedef struct frame {
-	size_t reg_base;
-	size_t ins;
-	tab *env;
-
-	func *func;
-} frame;
-
-RH_AL_MAKE(frame_stack, frame)
-
-
-typedef struct global {
-	frame_stack func;
-	val_al stack;
-	
-	// Mem management
-	size_t white;		// Current val of white tag (0, 1)
-	mem_block *gc_list;	// All objects
-} global;
+#include "core_api.h"
 
 int nua_print_val(int no_args, val *stack) {
 	if (!no_args) {
 		return 0;
 	}
 	
-	print_val(*stack);
+	print_val(stack[1]);
 	fflush(stdout);
 	return 0;
-}
-
-
-size_t gc_stack_top(global *g) {
-	frame *f = frame_stack_rpeek(&g->func);
-	size_t top_reg = f->func->def->gc_height.items[f->ins];
-	
-	//printf("%ld, %ld", f->reg_base, top_reg);
-	return f->reg_base + top_reg;
-}
-
-void gc_mark(global *g) {
-	g->white = !g->white;
-	int white = g->white;
-	
-	// FIXME get real top from instruction
-	for (int i = 0;i < gc_stack_top(g);++i) {
-		//printf("; %ld, i%d\n", gc_stack_top(g), i);
-		gc_val_mark(&g->stack.items[i], !white);
-	}
-	
-	// Functions should be marked on the stack
-	for (int i = 0;i < g->func.top;++i) {
-		gc_tab_mark(g->func.items[i].env, !white);
-	}
 }
 
 int main(int argn, char **args) {
@@ -76,9 +33,14 @@ int main(int argn, char **args) {
 	}
 
 	parse_init();
+	
+	nua_state *n = nua_init();
 
-	func *base = gc_alloc(&global_heap, sizeof(*base), GC_FUNC);
-	*base = (func) { .type = FUNC_NUA, .def = calloc(sizeof(*base->def), 1)};
+	func *base = gc_alloc(&global_heap, sizeof(*base), GC_FUNC); {
+		tab *env = gc_alloc(&global_heap, sizeof(tab), GC_TAB);
+		*env = (tab) {0};
+		*base = (func) {.type = FUNC_NUA, .def = gc_alloc(&global_heap, sizeof(*base->def), GC_FUNCDEF), .env = env};
+	}
 
 	if (parse((lexer){args[1], file, .lstart = file}, base->def) || parse_errors.items) {
 		fprintf(stderr, "Unable to parse file!\n");
@@ -88,240 +50,22 @@ int main(int argn, char **args) {
 
 		return 1;
 	}
-
+	free((void *) file);
+	
 	print_func_def(*base->def);
-
-	global g = {.stack = val_al_new(256), .func = frame_stack_new(8)}; {
-		for (int i = 0; i < g.stack.size;++i) {
-			g.stack.items[i] = (val) { VAL_NIL };
-		}
-		tab *env = gc_alloc(&global_heap, sizeof(tab), GC_TAB);
-		frame_stack_push(&g.func, (frame) {.func = base, .env = env, .reg_base = 0});
-	}
-
-	frame *top = frame_stack_rpeek(&g.func);
-
-	val *reg = &g.stack.items[top->reg_base];
-	val *lit = top->func->def->literals.items;
-	tab *env = top->env;
 	
 	func *print =  gc_alloc(&global_heap, sizeof(*print), GC_FUNC);
 	print->type = FUNC_C;
 	print->c_func = &nua_print_val;
 	
-	tab_set(env, (val){VAL_STR, .str = intern(&global_heap, &global_intern_map, (slice) {
+	tab_set(base->env, (val){VAL_STR, .str = intern(&global_heap, &global_intern_map, (slice) {
 		.len = 5,
 		.str = "print"})}, 
 		(val){VAL_FUNC, .func = print});
-
-	while (1) {
-		inst ins = top->func->def->ins.items[top->ins];
-		//print_inst(ins);
-		switch (ins.op) {
-		case OP_JMP:
-			top->ins += ins.off;
-			continue;	// Avoid addition at end of loop
-		case OP_COVER:
-			if (reg[ins.reg].type != VAL_NIL) {
-				top->ins++;
-			}
-			break;
-		case OP_NIL:
-			reg[ins.reg] = (val) {VAL_NIL};
-			break;
-		case OP_SETL:
-			switch (lit[ins.lit].type) {
-			case VAL_TAB:
-				reg[ins.reg] = (val) {VAL_TAB, .tab = gc_alloc(&global_heap, sizeof(tab), GC_TAB)};
-				reg[ins.reg].tab->al = val_al_clone(&lit[ins.lit].tab->al);
-				reg[ins.reg].tab->ht = val_ht_clone(&lit[ins.lit].tab->ht);
-				break;
-			case VAL_FUNC:
-				reg[ins.reg] = (val) {VAL_FUNC, .func = gc_alloc(&global_heap, sizeof(func), GC_FUNC)};
-				reg[ins.reg].func->type = FUNC_NUA;
-				reg[ins.reg].func->def = lit[ins.lit].func->def;
-				break;
-			default:
-				reg[ins.reg] = lit[ins.lit];
-				break;
-			}
-			break;
-		case OP_CALL:
-			if (reg[ins.rout].type != VAL_FUNC) {
-				printf("Attempt to call non-function!\n");
-				print_val(reg[ins.rout]);
-				return -1;
-			}
-			// OP is interpreted
-			// .rout = func register, and base of func args - 1, base of return vals
-			// .rina = no args, call has to pad with nils
-			// .rinb = no return vals, return has to pad with nils
-			frame_stack_push(&g.func, (frame) {.func = reg[ins.rout].func, .reg_base = &reg[ins.rout+1] - g.stack.items});
-			switch (reg[ins.rout].func->type) {
-			case FUNC_NUA: {
-
-				size_t max = g.stack.top + reg[ins.rout].func->def->max_reg;
-
-				if (max >= g.stack.size) {
-					size_t old = g.stack.size, new = g.stack.size * 2;
-					val_al_resize(&g.stack, new);
-					for (size_t i = old;i < new;++i) {
-						g.stack.items[i] = (val) { VAL_NIL };
-					}
-				}
-
-				for (int i = ins.rina;i < reg[ins.rout].func->def->no_args;++i) {
-					reg[ins.rout + i] = (val) { VAL_NIL };
-				}
-
-				top = frame_stack_rpeek(&g.func);
-
-				reg = &g.stack.items[top->reg_base];
-				lit = top->func->def->literals.items;
-				if (top->func->env) {
-					env = top->env = top->func->env;
-				} else {
-					env = (top-1)->env;
-				}
-			} break;
-			case FUNC_C: {	
-				top = frame_stack_rpeek(&g.func);
-				int no_ret = reg[ins.rout].func->c_func(ins.rina, &g.stack.items[top->reg_base]);
-				
-				for (int i = no_ret;i < ins.rinb;++i) {
-					reg[ins.rout + i] = (val) { VAL_NIL };
-				}
-				
-				frame_stack_pop(&g.func);
-				top = frame_stack_rpeek(&g.func);
-				
-				top->ins++;
-			} break;
-			default:
-				printf("ERROR: invalid function called\n");
-				return -1;
-			}
-			// Avoid skipping the first instruction
-			continue;
-		case OP_RET:
-			// OP is interpreted
-			// .rina = base register
-			// .rout = no values to return
-			frame_stack_pop(&g.func);
-			size_t no_ret = ins.rout;
-
-			reg -= 1;
-
-			for (int i = 0;i < no_ret;++i) {
-				reg[i] = reg[ins.rina + i + 1];
-			}
-
-			top = frame_stack_rpeek(&g.func);
-
-			reg = &g.stack.items[top->reg_base];
-			lit = top->func->def->literals.items;
-
-			ins = top->func->def->ins.items[top->ins];
-			for (int i = no_ret;i < ins.rinb;++i) {
-				reg[ins.rout + i] = (val) { VAL_NIL };
-			}
-
-			env = top->env;
-
-			break;
-		case OP_ADD:
-			if (reg[ins.rina].type == VAL_NUM
-			&&  reg[ins.rinb].type == VAL_NUM) {
-				reg[ins.rout] = (val) {VAL_NUM, reg[ins.rina].num + reg[ins.rinb].num};
-				break;
-			}
-			break;
-		case OP_SUB:
-			if (reg[ins.rina].type == VAL_NUM
-			&&  reg[ins.rinb].type == VAL_NUM) {
-				reg[ins.rout] = (val) {VAL_NUM, reg[ins.rina].num - reg[ins.rinb].num};
-				break;
-			}
-			break;
-		case OP_GT:
-			if (reg[ins.rina].type == VAL_NUM
-			&&  reg[ins.rinb].type == VAL_NUM) {
-				if (reg[ins.rina].num > reg[ins.rinb].num) {
-					reg[ins.rout] = reg[ins.rinb];
-				} else {
-					reg[ins.rout] = (val) {VAL_NIL};
-				}
-				break;
-			}
-			break;
-		case OP_GE:
-			if (reg[ins.rina].type == VAL_NUM
-			&&  reg[ins.rinb].type == VAL_NUM) {
-				if (reg[ins.rina].num >= reg[ins.rinb].num) {
-					reg[ins.rout] = reg[ins.rinb];
-				} else {
-					reg[ins.rout] = (val) {VAL_NIL};
-				}
-			}
-			break;
-		case OP_MOV:
-			reg[ins.rout] = reg[ins.rina];
-			break;
-		case OP_TAB:
-			reg[ins.rout] = (val) {VAL_TAB, .tab = gc_alloc(&global_heap, sizeof(tab), GC_TAB)};
-			val_ht_resize(&reg[ins.rout].tab->ht, ins.rina);
-			val_al_resize(&reg[ins.rout].tab->al, RH_HASH_SIZE(ins.rinb));
-			break;
-		case OP_PTAB:
-			switch (reg[ins.rout].type) {
-			case VAL_TAB:
-				tab_push(reg[ins.rout].tab, reg[ins.rina]);
-				break;
-			default:
-				break;
-			}
-			break;
-		case OP_STAB:
-			switch (reg[ins.rout].type) {
-			case VAL_TAB:
-				tab_set(reg[ins.rout].tab, reg[ins.rina], reg[ins.rinb]);
-				break;
-			default:
-				print_val(reg[ins.rout]);
-				break;
-			}
-			break;
-		case OP_GTAB:
-			switch (reg[ins.rina].type) {
-			case VAL_TAB:
-				reg[ins.rout] = tab_get(reg[ins.rina].tab, reg[ins.rinb]);
-				break;
-			default:
-				break;
-			}
-			break;
-		case OP_SENV:
-			tab_set(env, lit[ins.lit], reg[ins.reg]);
-			break;
-		case OP_GENV:
-			reg[ins.reg] = tab_get(env, lit[ins.lit]);
-			break;
-		case OP_END:
-			printf("Register 0; ");
-			print_val(reg[0]);
-
-			printf("Register 1; ");
-			print_val(reg[1]);
-
-			free((void *) file);
-			return 0;
-		default:
-			break;
-		}
-		gc_mark(&g);
-		gc_sweep(&global_heap, g.white);
-		top->ins++;
-	}
+		
+	val_al_push(&n->stack, (val) {VAL_FUNC, .func = base});
+		
+	nua_call(n, 0, 0, 0);
 
 	return 0;
 }
